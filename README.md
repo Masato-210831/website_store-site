01445d407fd4465e6631fcf55fccaa6cf6d2e8b9
masato-210831 <b86500b@gmail.com>
2022-02-03 22:16:32 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# website_store-site
+中級編：ストアサイト（インテリア）／グリッドレイアウト
+https://code-step.com/store2-menu/
---
+++ js/main.js
@@ -0,0 +1,56 @@
+'use strict'
+
+{
+  
+  const header = document.getElementById('header');
+  const btn1 = document.querySelector('.menu-btn');
+  const border = document.querySelectorAll('.menu-btn span');
+
+  const sideBarBtn = document.getElementById('sidebar-btn');
+  const sideBar = document.getElementById('sidebar');
+  const wrapper = document.querySelectorAll('.wrapper');
+  const overlay = document.querySelector('.overlay');
+
+
+  // ボタンを押す→黒背景にメニューが現れる
+  btn1.addEventListener('click', () => {
+    // headerに.openがあるかで条件分岐
+    if (header.classList.contains('open')) {
+      header.classList.remove('open');
+      border.forEach(border => {
+      border.style.background = '#000';
+    } );
+  } else {
+    header.classList.add('open');
+    border.forEach(border => {
+      border.style.background = '#fff'
+    } );
+  };
+})
+
+  // サイドバー関係の処理
+  sideBarBtn.addEventListener('click', () => {
+    if (sideBar.classList.contains('open')) {
+      sideBar.style.opacity = 0;
+      sideBar.style.left = '-200px';
+      sideBar.classList.remove('open');
+      sideBarBtn.classList.remove('open');
+    } else {
+      sideBar.classList.add('open');
+      sideBarBtn.classList.add('open');
+      sideBar.style.opacity = 1;
+      sideBar.style.left = 0;
+    }
+
+    overlay.addEventListener('click', () => {
+      sideBar.style.opacity = 0;
+      sideBar.style.left = '-200px';
+      sideBar.classList.remove('open');
+      sideBarBtn.classList.remove('open');
+    })
+  });
+
+
+
+}
+
